3cde1c1522a4f61ea5ba754f89d60ca705119e3a
shiye <shiyed@hotmail.com>
2025-02-03 00:23:14 +0200

    Refactor OnModelCreating method to improve entity configurations and relationships

--- Entities/User.cs
+++ Entities/User.cs
@@ -30,7 +30,7 @@ namespace motoMeet
         // A Person can have many UserRoute records
         public virtual ICollection<UserRoute> UserRoutes { get; set; } = new List<UserRoute>();
         public virtual ICollection<PersonFollow> Followers { get; set; } = new List<PersonFollow>();
-        public virtual ICollection<PersonFollow> Following  { get; set; } = new List<PersonFollow>();
+        public virtual ICollection<PersonFollow> Following { get; set; } = new List<PersonFollow>();
 
         // Example: If a Person can be in multiple groups, you'll handle that via GroupMember.
         public virtual ICollection<GroupMember> GroupMemberships { get; set; } = new List<GroupMember>();
@@ -104,73 +104,227 @@ namespace motoMeet
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Route>().Property(r => r.StartPoint).HasColumnType("geography");
-            modelBuilder.Entity<Route>().Property(r => r.EndPoint).HasColumnType("geography");
-            modelBuilder.Entity<RoutePoint>().Property(r => r.Point).HasColumnType("geography");
-            //             modelBuilder.Entity<RouteTag>()
-            //                  .HasKey(rt => new { rt.RouteId, rt.TagId });
-            //             modelBuilder.Entity<RoutesTypes>()
-            //  .HasKey(rt => new { rt.RouteId, rt.RouteTypeId });
+            base.OnModelCreating(modelBuilder);
 
-            // modelBuilder.Entity<RouteTag>()
-            //     .HasOne(rt => rt.Route)
-            //     .WithMany(r => r.RouteTags)
-            //     .HasForeignKey(rt => rt.RouteId);
+    // ---------------- Geographic types ----------------
+    modelBuilder.Entity<Route>()
+        .Property(r => r.StartPoint)
+        .HasColumnType("geography");
+    modelBuilder.Entity<Route>()
+        .Property(r => r.EndPoint)
+        .HasColumnType("geography");
 
-            // modelBuilder.Entity<RouteTag>()
-            //     .HasOne(rt => rt.Tag)
-            //     .WithMany(t => t.RouteTags)
-            //     .HasForeignKey(rt => rt.TagId);
+    modelBuilder.Entity<RoutePoint>()
+        .Property(rp => rp.Point)
+        .HasColumnType("geography");
 
+    modelBuilder.Entity<UserRoutePoint>()
+        .Property(urp => urp.Point)
+        .HasColumnType("geography");
 
-            modelBuilder
-                  .Entity<EventStage>()
-                  .Property(es => es.StageType)
-                  .HasConversion<string>(); // store enum as string
-            modelBuilder
-                   .Entity<PointOfInterest>()
-                   .Property(poi => poi.WaypointType)
-                   .HasConversion<string>();
-            modelBuilder.Entity<EventActivity>()
-                .HasKey(ea => new { ea.EventId, ea.ActivityTypeId });
+    modelBuilder.Entity<PointOfInterest>()
+        .Property(poi => poi.Location)
+        .HasColumnType("geography");
 
-            modelBuilder.Entity<EventActivity>()
-                .HasOne(ea => ea.Event)
-                .WithMany(e => e.EventActivities)
-                .HasForeignKey(ea => ea.EventId);
+    // ---------------- Enum Conversions ----------------
+    modelBuilder.Entity<EventStage>()
+        .Property(es => es.StageType)
+        .HasConversion<string>();
 
-            modelBuilder.Entity<EventActivity>()
-                .HasOne(ea => ea.ActivityType)
-                .WithMany(at => at.EventActivities)
-                .HasForeignKey(ea => ea.ActivityTypeId);
-            modelBuilder.Entity<GroupActivity>()
-            .HasKey(ga => new { ga.GroupId, ga.ActivityTypeId });
+    modelBuilder.Entity<PointOfInterest>()
+        .Property(poi => poi.WaypointType)
+        .HasConversion<string>();
 
-            modelBuilder.Entity<GroupActivity>()
-                .HasOne(ga => ga.Group)
-                .WithMany(g => g.GroupActivities)
-                .HasForeignKey(ga => ga.GroupId);
+    // ---------------- Composite Keys & Relationships ----------------
+    // EventActivity: many-to-many between Event and ActivityType
+    modelBuilder.Entity<EventActivity>()
+        .HasKey(ea => new { ea.EventId, ea.ActivityTypeId });
+    modelBuilder.Entity<EventActivity>()
+        .HasOne(ea => ea.Event)
+        .WithMany(e => e.EventActivities)
+        .HasForeignKey(ea => ea.EventId)
+        .OnDelete(DeleteBehavior.Cascade);
+    modelBuilder.Entity<EventActivity>()
+        .HasOne(ea => ea.ActivityType)
+        .WithMany(at => at.EventActivities)
+        .HasForeignKey(ea => ea.ActivityTypeId)
+        .OnDelete(DeleteBehavior.Cascade);
 
-            modelBuilder.Entity<GroupActivity>()
-                .HasOne(ga => ga.ActivityType)
-                .WithMany(at => at.GroupActivities)
-                .HasForeignKey(ga => ga.ActivityTypeId);
-            modelBuilder.Entity<EventStageParticipant>()
-             .HasOne(esp => esp.EventStage)
-             .WithMany(es => es.StageParticipants)
-             .HasForeignKey(esp => esp.EventStageId);
+    // GroupActivity: many-to-many between Group and ActivityType
+    modelBuilder.Entity<GroupActivity>()
+        .HasKey(ga => new { ga.GroupId, ga.ActivityTypeId });
+    modelBuilder.Entity<GroupActivity>()
+        .HasOne(ga => ga.Group)
+        .WithMany(g => g.GroupActivities)
+        .HasForeignKey(ga => ga.GroupId)
+        .OnDelete(DeleteBehavior.Cascade);
+    modelBuilder.Entity<GroupActivity>()
+        .HasOne(ga => ga.ActivityType)
+        .WithMany(at => at.GroupActivities)
+        .HasForeignKey(ga => ga.ActivityTypeId)
+        .OnDelete(DeleteBehavior.Cascade);
 
-            modelBuilder.Entity<EventStageParticipant>()
-                .HasOne(esp => esp.EventParticipant)
-                .WithMany(ep => ep.StageParticipants)
-                .HasForeignKey(esp => esp.EventParticipantId);
+    // ---------------- Person & PersonFollow ----------------
+    // PersonFollow primary key and relationships
+    modelBuilder.Entity<PersonFollow>()
+        .HasKey(pf => pf.Id);
+    modelBuilder.Entity<PersonFollow>()
+        .HasOne(pf => pf.Follower)
+        .WithMany(p => p.Following) // Person.Following: persons that this user is following
+        .HasForeignKey(pf => pf.FollowerId)
+        .OnDelete(DeleteBehavior.Cascade);
+    modelBuilder.Entity<PersonFollow>()
+        .HasOne(pf => pf.Following)
+        .WithMany(p => p.Followers) // Person.Followers: persons following this user
+        .HasForeignKey(pf => pf.FollowingId)
+        .OnDelete(DeleteBehavior.Cascade);
 
-            modelBuilder.Entity<EventStageParticipant>()
-                .HasOne(esp => esp.UserRoute)
-                .WithOne(ur=>ur.EventStageParticipant)  // or with .WithMany(ur => ur.EventStageParticipants) if you want a back-collection
-                .HasForeignKey<EventStageParticipant>(esp => esp.UserRouteId);  
+    // ---------------- Notification, Reaction, Favorite ----------------
+    modelBuilder.Entity<Notification>()
+        .HasOne(n => n.Recipient)
+        .WithMany() // Optionally add a collection navigation on Person
+        .HasForeignKey(n => n.RecipientId)
+        .OnDelete(DeleteBehavior.Cascade);
+    modelBuilder.Entity<Notification>()
+        .HasOne(n => n.Actor)
+        .WithMany() // Optionally add a collection navigation on Person
+        .HasForeignKey(n => n.ActorId)
+        .OnDelete(DeleteBehavior.Restrict);
 
- 
+    modelBuilder.Entity<Reaction>()
+        .HasOne(r => r.Person)
+        .WithMany() // Optionally add a collection navigation on Person
+        .HasForeignKey(r => r.PersonId)
+        .OnDelete(DeleteBehavior.Cascade);
+
+    modelBuilder.Entity<Favorite>()
+        .HasOne(f => f.Person)
+        .WithMany() // Optionally add a collection navigation on Person
+        .HasForeignKey(f => f.PersonId)
+        .OnDelete(DeleteBehavior.Cascade);
+
+    // ---------------- Route, Reviews, and UserRoute ----------------
+    // Route: One-to-many with Reviews
+    modelBuilder.Entity<Review>()
+        .HasOne(r => r.Route)
+        .WithMany(rt => rt.Reviews)
+        .HasForeignKey(r => r.RouteId)
+        .OnDelete(DeleteBehavior.Cascade);
+
+    // UserRoute: relationships with Person and Route
+    modelBuilder.Entity<UserRoute>()
+        .HasOne(ur => ur.Person)
+        .WithMany()  // Optionally add a navigation property on Person
+        .HasForeignKey(ur => ur.PersonId)
+        .OnDelete(DeleteBehavior.Cascade);
+    modelBuilder.Entity<UserRoute>()
+        .HasOne(ur => ur.Route)
+        .WithMany(r => r.UserRoutes)
+        .HasForeignKey(ur => ur.RouteId)
+        .OnDelete(DeleteBehavior.Cascade);
+
+    // ---------------- Event and Its Child Entities ----------------
+    modelBuilder.Entity<Event>()
+        .HasOne(e => e.Creator)
+        .WithMany() // Optionally add a collection navigation on Person (e.g. CreatedEvents)
+        .HasForeignKey(e => e.CreatorId)
+        .OnDelete(DeleteBehavior.Restrict);
+    modelBuilder.Entity<Event>()
+        .HasOne(e => e.Group)
+        .WithMany(g => g.Events)
+        .HasForeignKey(e => e.GroupId)
+        .OnDelete(DeleteBehavior.SetNull);
+
+    modelBuilder.Entity<EventStage>()
+        .HasOne(es => es.Event)
+        .WithMany(e => e.Stages)
+        .HasForeignKey(es => es.EventId)
+        .OnDelete(DeleteBehavior.Cascade);
+    modelBuilder.Entity<EventStage>()
+        .HasOne(es => es.Route)
+        .WithMany() // Route may not need a navigation back here
+        .HasForeignKey(es => es.RouteId)
+        .OnDelete(DeleteBehavior.SetNull);
+
+    modelBuilder.Entity<EventParticipant>()
+        .HasOne(ep => ep.Event)
+        .WithMany(e => e.Participants)
+        .HasForeignKey(ep => ep.EventId)
+        .OnDelete(DeleteBehavior.Cascade);
+    modelBuilder.Entity<EventParticipant>()
+        .HasOne(ep => ep.Person)
+        .WithMany() // Optionally add a navigation property on Person
+        .HasForeignKey(ep => ep.PersonId)
+        .OnDelete(DeleteBehavior.Restrict);
+
+    modelBuilder.Entity<EventStageParticipant>()
+        .HasOne(esp => esp.EventStage)
+        .WithMany(es => es.StageParticipants)
+        .HasForeignKey(esp => esp.EventStageId)
+        .OnDelete(DeleteBehavior.Cascade);
+    modelBuilder.Entity<EventStageParticipant>()
+        .HasOne(esp => esp.EventParticipant)
+        .WithMany(ep => ep.StageParticipants)
+        .HasForeignKey(esp => esp.EventParticipantId)
+        .OnDelete(DeleteBehavior.Cascade);
+    modelBuilder.Entity<EventStageParticipant>()
+        .HasOne(esp => esp.UserRoute)
+        .WithMany() // No navigation property assumed on UserRoute
+        .HasForeignKey(esp => esp.UserRouteId)
+        .OnDelete(DeleteBehavior.SetNull);
+
+    modelBuilder.Entity<EventItem>()
+        .HasOne(ei => ei.Event)
+        .WithMany(e => e.RequiredItems)
+        .HasForeignKey(ei => ei.EventId)
+        .OnDelete(DeleteBehavior.Cascade);
+
+    // ---------------- Group and Its Related Entities ----------------
+    modelBuilder.Entity<Group>()
+        .HasOne(g => g.Creator)
+        .WithMany() // Optionally add a navigation property on Person
+        .HasForeignKey(g => g.CreatorId)
+        .OnDelete(DeleteBehavior.Restrict);
+
+    modelBuilder.Entity<GroupMember>()
+        .HasOne(gm => gm.Group)
+        .WithMany(g => g.Members)
+        .HasForeignKey(gm => gm.GroupId)
+        .OnDelete(DeleteBehavior.Cascade);
+    modelBuilder.Entity<GroupMember>()
+        .HasOne(gm => gm.Person)
+        .WithMany() // Optionally add a navigation property on Person
+        .HasForeignKey(gm => gm.PersonId)
+        .OnDelete(DeleteBehavior.Cascade);
+
+    modelBuilder.Entity<GroupPost>()
+        .HasOne(gp => gp.Group)
+        .WithMany(g => g.Posts)
+        .HasForeignKey(gp => gp.GroupId)
+        .OnDelete(DeleteBehavior.Cascade);
+    modelBuilder.Entity<GroupPost>()
+        .HasOne(gp => gp.Author)
+        .WithMany() // Optionally add a navigation property on Person (e.g. Posts)
+        .HasForeignKey(gp => gp.AuthorId)
+        .OnDelete(DeleteBehavior.Restrict);
+
+    modelBuilder.Entity<GroupPostComment>()
+        .HasOne(gpc => gpc.GroupPost)
+        .WithMany(gp => gp.Comments)
+        .HasForeignKey(gpc => gpc.GroupPostId)
+        .OnDelete(DeleteBehavior.Cascade);
+    modelBuilder.Entity<GroupPostComment>()
+        .HasOne(gpc => gpc.Author)
+        .WithMany() // Optionally add a navigation property on Person
+        .HasForeignKey(gpc => gpc.AuthorId)
+        .OnDelete(DeleteBehavior.Restrict);
+
+    modelBuilder.Entity<GroupPostAttachment>()
+        .HasOne(gpa => gpa.GroupPost)
+        .WithMany(gp => gp.Attachments)
+        .HasForeignKey(gpa => gpa.GroupPostId)
+        .OnDelete(DeleteBehavior.Cascade);
+
 
         }
         public MotoMeetDbContext(DbContextOptions<MotoMeetDbContext> options) : base(options)
